702cf67db14e8408209c26257a5fe30be7d7458a
Bruno Osorio <osorio.bruno@hotmail.com>
2019-03-25 20:47:06 -0300

    Proper handling of grayscale images, output of video is also played

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,15 @@
 void showUsage();
 void openInput(cv::String filePath);
 void onMouseEvent(int event, int x, int y, int flags, void* userdata);
-void makePixelSelection();
+void segmentImageByPixelColor();
 bool isWindowOpen(const char* windowName);
 
 cv::VideoCapture g_videoCapture;
 cv::Mat g_imageInput;
 cv::Mat g_imageOutput;
 
+cv::Vec3b g_clickedPixelColorRGB;
+uint8_t g_clickedPixelColorGray;
 int32_t g_videoFrameCount;
 
 bool g_isInputImage = false;
@@ -24,13 +26,15 @@ bool g_isInputVideo = false;
 bool g_isVideoPaused = false;
 bool g_isImageGrayscale = false;
 bool g_hasClicked = false;
+bool g_renderOutput = false;
 
 cv::Point g_mouseClickPoint;
 
 int main(int argc, char** argv)
 {
     setvbuf(stdout, nullptr, _IONBF, 0);
-    cv::String firstArg;
+
+    std::vector<cv::String> args(argv, argv + argc);
 
     if (argc <= 1)
     {
@@ -38,14 +42,12 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    firstArg = argv[1];
-
-    if (firstArg == "-h" || firstArg == "--help")
+    if (args[1] == "-h" || args[1] == "--help")
     {
         showUsage();
         return 0;
     }
-    else if (firstArg == "cam" || firstArg == "webcam")
+    else if (args[1] == "cam" || args[1] == "webcam")
     {
         g_videoCapture.open(0);
 
@@ -57,21 +59,21 @@ int main(int argc, char** argv)
     }
     else
     {
-        std::ifstream inputFile(firstArg.c_str());
+        std::ifstream inputFile(args[1].c_str());
 
         if (!inputFile.is_open())
         {
-            printf("Couldn't open input file \"%s\".\n", firstArg.c_str());
+            printf("Couldn't open input file \"%s\".\n", args[1].c_str());
             return 0;
         }
 
         inputFile.close();
 
-        openInput(firstArg);
+        openInput(args[1]);
 
         if (!g_isInputImage && !g_isInputVideo)
         {
-            printf("Input file \"%s\" wasn't recognized as image or video.\n", firstArg.c_str());
+            printf("Input file \"%s\" wasn't recognized as image or video.\n", args[1].c_str());
             return 0;
         }
     }
@@ -127,17 +129,24 @@ int main(int argc, char** argv)
                 g_isVideoPaused = !g_isVideoPaused;
         }
 
-        if (g_isInputVideo && secondsSinceLastDraw >= fpsInv)
+        if (g_isInputVideo)
         {
-            secondsSinceLastDraw -= fpsInv;
-            g_videoCapture.read(g_imageInput);
-            cv::imshow(WINDOW_TITLE_INPUT, g_imageInput);
+            if (secondsSinceLastDraw >= fpsInv)
+            {
+                secondsSinceLastDraw -= fpsInv;
+                g_videoCapture.read(g_imageInput);
+                cv::imshow(WINDOW_TITLE_INPUT, g_imageInput);
+
+                if (g_hasClicked)
+                    g_renderOutput = true;
+            }
         }
 
-        if (g_hasClicked)
+        if (g_renderOutput)
         {
-            g_hasClicked = false;
-            makePixelSelection();
+            g_renderOutput = false;
+
+            segmentImageByPixelColor();
 
             if (!isWindowOpen(WINDOW_TITLE_OUTPUT))
                 cv::namedWindow(WINDOW_TITLE_OUTPUT, windowFlags);
@@ -159,6 +168,10 @@ void showUsage()
     printf("Usage:\n");
     printf(" ./project1 <image or video file path>\n");
     printf(" ./project1 webcam\n");
+    printf("\n");
+    printf("Use the mouse to click on any pixel. Every pixel\n");
+    printf("that is within a certain threshold will be paint\n");
+    printf("with the color red in a new screen that will open.\n");
 }
 
 void openInput(cv::String filePath)
@@ -168,7 +181,7 @@ void openInput(cv::String filePath)
     if (g_imageInput.rows > 0 && g_imageInput.cols > 0)
     {
         int32_t channels = g_imageInput.channels();
-        printf("Input image has %d channels.\n", channels);
+        printf("Input image has %d channel(s).\n", channels);
         g_isInputImage = true;
         g_isImageGrayscale = channels <= 2;
         return;
@@ -199,46 +212,62 @@ void onMouseEvent(int32_t event, int32_t x, int32_t y, int32_t flags, void* user
     {
         g_mouseClickPoint = cv::Point(x, y);
         g_hasClicked = true;
+        g_renderOutput = true;
+
+        if (g_isImageGrayscale)
+        {
+            g_clickedPixelColorGray = g_imageInput.at<uint8_t>(g_mouseClickPoint);
+            printf(
+               "Clicked pixel: x/y = %d/%d; Gray Intensity = %d\n",
+               g_mouseClickPoint.x, g_mouseClickPoint.y,
+               g_clickedPixelColorGray
+            );
+        }
+        else
+        {
+            g_clickedPixelColorRGB = g_imageInput.at<cv::Vec3b>(g_mouseClickPoint);
+            printf(
+               "Clicked pixel: x/y = %d/%d; R/G/B = %d/%d/%d\n",
+               g_mouseClickPoint.x, g_mouseClickPoint.y,
+               g_clickedPixelColorRGB.val[2], g_clickedPixelColorRGB.val[1], g_clickedPixelColorRGB.val[0]
+            );
+        }
     }
 }
 
-void makePixelSelection()
+void segmentImageByPixelColor()
 {
     constexpr int32_t threshold = COLOR_THRESHOLD * COLOR_THRESHOLD;
-    cv::Vec3b pixelColorRGB;
 
     g_imageOutput = g_imageInput.clone();
 
     if (g_isImageGrayscale)
     {
-        uint8_t pixelColorGray = g_imageInput.at<uint8_t>(g_mouseClickPoint);
-        printf(
-               "Clicked pixel: x/y = %d/%d; Gray Intensity = %d\n",
-               g_mouseClickPoint.x, g_mouseClickPoint.y,
-               pixelColorGray
-        );
-        cv::cvtColor(g_imageOutput, g_imageOutput, cv::COLOR_GRAY2RGB);
-        pixelColorRGB = cv::Vec3b(pixelColorGray, pixelColorGray, pixelColorGray);
+        g_imageOutput.forEach<uint8_t>([&](uint8_t& p, const int position[]) -> void {
+            int32_t diff = static_cast<int32_t>(p) - static_cast<int32_t>(g_clickedPixelColorGray);
+            diff = diff * diff;
+            p = (diff < threshold) * 255;
+        });
+
+        cv::Mat channels[3];
+        cv::cvtColor(g_imageOutput, g_imageOutput, cv::COLOR_GRAY2BGR, 3);
+        cv::split(g_imageOutput, channels);
+        channels[0] *= 0;
+        channels[1] *= 0;
+        cv::merge(channels, 3, g_imageOutput);
     }
     else
     {
-        pixelColorRGB = g_imageInput.at<cv::Vec3b>(g_mouseClickPoint);
-        printf(
-               "Clicked pixel: x/y = %d/%d; R/G/B = %d/%d/%d\n",
-               g_mouseClickPoint.x, g_mouseClickPoint.y,
-               pixelColorRGB.val[2], pixelColorRGB.val[1], pixelColorRGB.val[0]
-        );
+        g_imageOutput.forEach<cv::Vec3b>([&](cv::Vec3b& p, const int position[]) -> void {
+            int32_t diff0 = static_cast<int32_t>(p.val[0]) - static_cast<int32_t>(g_clickedPixelColorRGB.val[0]);
+            int32_t diff1 = static_cast<int32_t>(p.val[1]) - static_cast<int32_t>(g_clickedPixelColorRGB.val[1]);
+            int32_t diff2 = static_cast<int32_t>(p.val[2]) - static_cast<int32_t>(g_clickedPixelColorRGB.val[2]);
+            diff0 = diff0 * diff0 + diff1 * diff1 + diff2 * diff2;
+            p.val[0] = 0;
+            p.val[1] = 0;
+            p.val[2] = (diff0 < threshold) * 255;
+        });
     }
-
-    g_imageOutput.forEach<cv::Vec3b>([&](cv::Vec3b& p, const int position[]) -> void {
-        int32_t diff0 = static_cast<int32_t>(p.val[0]) - static_cast<int32_t>(pixelColorRGB.val[0]);
-        int32_t diff1 = static_cast<int32_t>(p.val[1]) - static_cast<int32_t>(pixelColorRGB.val[1]);
-        int32_t diff2 = static_cast<int32_t>(p.val[2]) - static_cast<int32_t>(pixelColorRGB.val[2]);
-        diff0 = diff0 * diff0 + diff1 * diff1 + diff2 * diff2;
-        p.val[0] = 0;
-        p.val[1] = 0;
-        p.val[2] = (diff0 < threshold) * 255;
-    });
 }
 
 bool isWindowOpen(const char* windowName)
